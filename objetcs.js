3a4fa340ad2b819fd50118f5fa23605ebb7b2757
lê <112022669+lecristinaa@users.noreply.github.com>
2024-02-08 12:43:21 -0300

    Create objetcs.js

---
+++ objetcs.js
@@ -0,0 +1,97 @@
+let alunos = [
+    {
+        nome: "Isabelle",
+        idade: 17,
+        ra: "245879",
+        cursando: true,
+        materiasCursadas: [
+            {
+                nome: "TIA",
+                professor: "Jorge"
+            },
+            {
+                nome: "ES",
+                professor: "Guilherme"
+            },
+        ]
+    },
+    {
+        nome: "Graziella",
+        idade: 15,
+        ra: "264878",
+        cursando: true,
+        materiasCursadas: [
+            {
+                nome: "Farmacologia",
+                professor: "Odair"
+            },
+            {
+                nome: "Regras Farmacêuticas",
+                professor: "Rita"
+            },
+        ]
+    }
+]
+
+console.log(alunos.materiasCursadas)
+
+
+
+
+
+/* const alunos = [
+    {
+        nome: "Isabelle",
+        idade: 16,
+        ra: "225064"
+    },
+
+    {
+        nome: "Marcia",
+        idade: 25,
+        ra: "226471"
+    },
+
+    {
+        nome: "Tais",
+        idade: 18,
+        ra: "235478"
+    },
+
+    {
+        nome: "Paula",
+        idade: 17,
+        ra: "234875"
+    },
+
+    {
+        nome: "Alessandra",
+        idade: 17,
+        ra:"224587"
+    },
+
+    {
+        nome: "Gabriela",
+        idade: 28,
+        ra:"254789"
+    }
+]
+
+console.log(alunos)
+*/
+
+
+/* let person = {
+        nome: "Matheus",
+        idade: 17,
+        ra: "22222",
+        materiasCursadas: ["TIA", "DPM"]
+    }
+    
+    console.log(person.idade)
+    
+    person.idade = 18
+    console.log(person.idade)
+    
+    console.log(person.materiasCursadas[1])
+*/
